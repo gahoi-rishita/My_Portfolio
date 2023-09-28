54747a4b3a68c05a533779cc0b6211a032a832c5
Rishita Gahoi <rishitagahoi5@gmail.com>
2023-09-28 16:08:53 +0530

    Further Procedure

---
+++ README.md
@@ -0,0 +1,6 @@
+# Portfolio-Website
+This website is my portfolio website . I use technology to made this is HTML , CSS &amp; Java Script.
+
+<p>If you want to see my portfolio you can visit it.</p>
+<h1><a href = "https://decoder2201.github.io/Portfolio-Website/">Live Preview</a></h1>
+
---
+++ script.js
@@ -0,0 +1,7 @@
+let a = document.querySelectorAll(".portfolio-image-container");
+a[0].addEventListener('click', function(){
+        document.location = "https://gahoi-rishita.github.io/Frontend-PG-Life/";
+    });
+a[1].addEventListener('click', function(){
+        document.location = "https://gahoi-rishita.github.io/City_Restaurant/";
+    });
